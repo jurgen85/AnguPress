ae97b32ddbceafe379c9615d5e7e4c3b65c9687b
Jurgen de Vries <jurgennn85@gmail.com>
2015-06-08 20:34:12 +0200

    deleted posts.js, no longer needed

--- js/services/posts.js
+++
@@ -1,9 +0,0 @@
-app.factory('posts', ['$http', function($http) {
-  return $http.get('http://localhost/user-feedback-rest-api/wp-json/posts')
-     .success(function(data) {
-       return data;
-     })
-     .error(function(data) {
-       return data;
-     });
-}]);
