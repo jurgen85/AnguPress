e01a0dda335d52ce41fc630addb040e218c702d5
Jurgen de Vries <jdv@fundament.nl>
2015-06-09 12:34:05 +0200

    Test with different callback to api url posts to make deleting a post possible.

--- js/controllers/PostController.js
+++ js/controllers/PostController.js
@@ -1,5 +1,17 @@
 app.controller('PostCtrl', ['$scope', 'post', '$routeParams', function($scope, post, $routeParams) {
+  // Original post binding to $scope.... try this new way of binding data to the $scope which may also give the ability to delete a post....?
   post.get({ id: $routeParams.id}, function(data) {
     $scope.post = data;
   });
+	
+  // Test if this works at home!!
+  /*var currentPost = post.get({ id: $routeParams.id}, function(data) {
+	currentPost.data = data;  
+  });
+  
+  $scope.post = currentPost.data;
+  
+  $scope.deletePost = function() {
+	  currentPost.$delete();
+  };*/
 }]);
